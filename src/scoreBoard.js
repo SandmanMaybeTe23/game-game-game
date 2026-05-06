9234146e17a39aa6aec1d35f10118c9b3eeec36d
billy <billy.burlin@elev.ga.ntig.se>
2026-05-06 13:50:26 +0200

    yea

--- src/scoreBoard.js
+++ src/scoreBoard.js
@@ -29,22 +29,40 @@ export default class ScoreBoard extends GameObject {
         }
 
 
-
-        
-
-        
-        
-        
-
-
     }
+
+
+
 
     draw(ctx) {
         // Rita rektangeln
         ctx.fillStyle = this.color
         ctx.fillRect(this.x, this.y, this.width, this.height)
 
-
-        
     }
+
+
+
+    async function getData() {
+    const url = "https://example.org/products.json";
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        console.log(result);
+    } catch (error) {
+        console.error(error.message);
+    }
+
+
 }
+
+
+
+
+
+
+
